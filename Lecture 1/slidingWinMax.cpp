f7b70ab040df272a4adfe456a4704e2eaa6b880b
Eleonora <eleonora.digregorio8@gmail.com>
2019-09-23 14:37:45 +0200

    sliding window with deque

--- Lecture 1/slidingWinMax.cpp
+++ Lecture 1/slidingWinMax.cpp
@@ -1,14 +1,31 @@
 #include <iostream>
-#include <algorithm>
 #include <vector>
+#include <deque>
 
 std::vector<int> maxinSub( std::vector<int> const &v, int k){
-    std::vector<int> max;
-    max.reserve( )
-    std::make_heap(v.begin(), v.begin()+k );
-    for(int i = k-1; i < n; i++ ){
+    std::vector<int> res;
+    res.reserve(v.size()-k+1);
+    std::deque< std::pair<int,int> > d;
+    for(int i = 0; i < v.size() ; i++ ){
 
+        if (d.size() > 0 ){
+
+            if (i - d.front().second > k ){
+                d.pop_front();
+            }
+            while(d.size() > 0 && v[i] > d.back().first){
+                d.pop_back();
+            }
+        }
+
+        std::pair <int, int> p(v[i],i);
+        d.push_back(p);
+
+        if(i >= k-1){
+            res.push_back(d.front().first);
+        }
     }
+    return res;
 }
 
 int main(){
@@ -24,8 +41,11 @@ int main(){
             std::cin >> aux;
             v.push_back(aux);
         }
-        int res = maxinSub( v,k );
-        std::cout << res << std::endl;
+        std::vector<int> res = maxinSub( v, k );
+        for(std::vector<int>::const_iterator it = res.begin(); it!= res.end(); it++ ){
+            std::cout << *it << " ";
+        }
+        std::cout << std::endl;
         v.clear();
     }
 }
