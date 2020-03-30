0df5e32bbf3420008abef8783ed45feefbbcd6be
eleonoradgr <eleonora.digregorio8@gmail.com>
2020-03-30 19:34:25 +0200

    maxPathSum added

--- Lecture04/BSTcheck.cpp
+++ Lecture04/BSTcheck.cpp
@@ -1,50 +1,54 @@
 #include <iostream>
 
 using namespace std;
+
 /* A binary tree node has data, pointer to left child
    and a pointer to right child */
 struct Node {
     int data;
-    Node* right;
-    Node* left;
+    Node *right;
+    Node *left;
 
-    Node(int x){
+    Node(int x) {
         data = x;
         right = NULL;
         left = NULL;
     }
 };
 
-pair<bool,pair<int,int>> checkBST( Node* tree){
-    if (tree->right == NULL && tree->left == NULL){
+pair<bool, pair<int, int>> checkBST(Node *tree) {
+    if (tree->right == NULL && tree->left == NULL) { //leaf of tree
         return make_pair(true, make_pair(tree->data, tree->data));
-    }else if(tree->right != NULL && tree->left == NULL){
-        pair<bool,pair<int,int>> rsubtree = checkBST(tree->right);
+    } else if (tree->right != NULL && tree->left == NULL) {
+        pair<bool, pair<int, int>> rsubtree = checkBST(tree->right);
         //check if right subtree is BST and data of current node is smaller than the minimum in right subtree
         bool isbst = rsubtree.first && (tree->data <= rsubtree.second.first);
-        pair<int,int> minmax = make_pair(min(rsubtree.second.first, tree->data),max(rsubtree.second.second, tree->data));
+        pair<int, int> minmax = make_pair(min(rsubtree.second.first, tree->data),
+                                          max(rsubtree.second.second, tree->data));
         return make_pair(isbst, minmax);
-    }else if(tree->right == NULL && tree->left != NULL){
-        pair<bool,pair<int,int>> lsubtree = checkBST(tree->left);
+    } else if (tree->right == NULL && tree->left != NULL) {
+        pair<bool, pair<int, int>> lsubtree = checkBST(tree->left);
         //check if left subtree is BST and data of current node is greater than the maximum in left subtree
         bool isbst = lsubtree.first && (tree->data > lsubtree.second.second);
-        pair<int,int> minmax = make_pair(min(lsubtree.second.first, tree->data),max(lsubtree.second.second, tree->data));
+        pair<int, int> minmax = make_pair(min(lsubtree.second.first, tree->data),
+                                          max(lsubtree.second.second, tree->data));
         return make_pair(isbst, minmax);
-    }else{
-        pair<bool,pair<int,int>> rsubtree = checkBST(tree->right);
-        pair<bool,pair<int,int>> lsubtree = checkBST(tree->left);
-        bool isbst = lsubtree.first && (tree->data > lsubtree.second.second) && rsubtree.first && (tree->data <= rsubtree.second.first);
-        pair<int,int> minmax = make_pair(min(min(lsubtree.second.first,rsubtree.second.first), tree->data),
-                max(max(lsubtree.second.second,rsubtree.second.second),tree->data));
+    } else {
+        pair<bool, pair<int, int>> rsubtree = checkBST(tree->right);
+        pair<bool, pair<int, int>> lsubtree = checkBST(tree->left);
+        bool isbst = lsubtree.first && (tree->data > lsubtree.second.second) && rsubtree.first &&
+                     (tree->data <= rsubtree.second.first);
+        pair<int, int> minmax = make_pair(min(min(lsubtree.second.first, rsubtree.second.first), tree->data),
+                                          max(max(lsubtree.second.second, rsubtree.second.second), tree->data));
         return make_pair(isbst, minmax);
     }
 }
 
-bool isBST(Node* root) {
-    pair<bool,pair<int,int>> result = checkBST( root);
+bool isBST(Node *root) {
+    pair<bool, pair<int, int>> result = checkBST(root);
     return result.first;
 }
 
-int main(){
+int main() {
     return 0;
 }
---
+++ Lecture04/maxPathSum.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <climits>
+
+/* A binary tree node has data, pointer to left child
+   and a pointer to right child */
+struct Node {
+    int data;
+    Node *right;
+    Node *left;
+
+    Node(int x) {
+        data = x;
+        right = NULL;
+        left = NULL;
+    }
+};
+
+std::pair<int, int> maxPathSumRec(Node *node) {
+    if (node->right == NULL && node->left == NULL)
+        return std::make_pair(INT_MIN, node->data);
+    std::pair<int, int> resR, resL = std::make_pair(NULL, NULL);
+    if (node->right != NULL)
+        resR = maxPathSumRec(node->right);
+    if (node->left != NULL)
+        resL = maxPathSumRec(node->left);
+    int maxsum, partial = 0;
+    if (node->right != NULL && node->left != NULL) {
+        int path = resR.second + resL.second + node->data;
+        maxsum = std::max(std::max(resR.first, resL.first), path);
+        partial = std::max(resR.second, resL.second) + node->data;
+    } else if (node->right != NULL) {
+        maxsum = resR.first;
+        partial = resR.second + node->data;
+    } else {
+        maxsum = resL.first;
+        partial = resL.second + node->data;
+    }
+    return std::make_pair(maxsum,partial);
+
+}
+
+int maxPathSum(Node *root) {
+    std::pair<int, int> result = maxPathSumRec(root);
+    return result.first;
+}
+
+int main() {
+    return 0;
+}
