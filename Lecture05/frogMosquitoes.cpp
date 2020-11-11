26bb1ebfa0ce05edd6f47e4f4b5ce138fd01a98b
eleonora <eleonora.digregorio8@gmail.com>
2020-11-11 21:46:49 +0100

    not working on test 4

--- Lecture05/frogMosquitoes.cpp
+++ Lecture05/frogMosquitoes.cpp
@@ -6,91 +6,114 @@
 #include <algorithm>
 #include <map>
 
+struct frog{
+    int number;
+    int pos;
+    int length;
+};
 
-bool myfunction(std::pair<int, int> i, std::pair<int, int> j){
-    return i.first < j.first;
+
+bool myfunction(frog &i, frog &j){
+    return i.pos < j.pos;
+}
+int find (std::vector<frog> &frogs, int l, int r, int pos){
+    while ( l <= r ){
+        int p = l + ( l -r )/ 2;
+        if ( frogs[p].pos == pos)
+            return p;
+        if (frogs[p].pos < pos )
+            l = p+1;
+        else
+            r = r-1;
+    }
+    return -1;
 }
 
 int main() {
-    //std::ios_base::sync_with_stdio(false);
+    std::ios_base::sync_with_stdio(false);
 
     int n = 0;
     int m = 0;
     std::cin >> n;
     std::cin >> m;
-    // frog data: first.first-> initial position of frog, first.second-> original length of its tongue
-    // second the ith frog
-    std::vector<std::pair<int, int>> frog_data;
-    // mosquitoes eaten by the ith frog, length of the tongue
+    // frog data: frog.number-> i-th frog, frog.pos-> initial position of frog, first.lenght-> original length of its tongue
+    std::vector<frog> frog_data;
+    // first -> mosquitoes eaten by the ith frog, second -> length of the tongue
     std::vector<std::pair<int, int>> eaten;
     frog_data.reserve(n);
     eaten.reserve(n);
     // frogs are seen as non overlapping segments,
-    // to keep the value of the frog another map is used
-    std::map<int, int> frogs;
-    std::map<int, int> key_val;
+    std::map<int, frog> frogs;
     // bst to keep track of uneaten mosquitoes
     std::map<int, int> mosquitoes;
     for (int i = 0; i < n; ++i) {
-        int start = 0;
-        int length = 0;
-        std::cin >> start;
-        std::cin >> length;
-        frog_data.emplace_back(std::make_pair(start, length));
-        key_val.insert(std::make_pair(start, i));
-        eaten.emplace_back(std::make_pair(0, length));
+        frog f;
+        f.number = i;
+        f.pos = 0;
+        f.length = 0;
+        std::cin >> f.pos;
+        std::cin >> f.length;
+        frog_data.emplace_back(f);
+        eaten.emplace_back(std::make_pair(0, f.length));
     }
     std::sort(frog_data.begin(), frog_data.end(), myfunction);
+    // creation of bbst where frog
     for (int i = 0; i < n; ++i) {
         if (frogs.empty()) {
-            frogs.insert(std::make_pair(frog_data[i].first, frog_data[i].first + frog_data[i].second));
+            frogs.insert(std::make_pair(frog_data[i].pos, frog_data[i]));
         } else {
-            auto pred = (frogs.lower_bound(frog_data[i].first))--;
-            if (pred->second < frog_data[i].first) {
-                frogs.insert(std::make_pair(frog_data[i].first, frog_data[i].first + frog_data[i].second));
-            } else if (pred->second - frog_data[i].first < frog_data[i].second) {
-                frogs.insert(std::make_pair(pred->second + 1, frog_data[i].first + frog_data[i].second));
+            auto pred = std::prev(frogs.lower_bound(frog_data[i].pos));
+            if (pred->first + pred->second.length < frog_data[i].pos) {
+                frogs.insert(std::make_pair(frog_data[i].pos, frog_data[i]));
+            } else if (pred->first + pred->second.length  < frog_data[i].pos + frog_data[i].length) {
+                frogs.insert(std::make_pair(pred->first + pred->second.length +1 ,frog_data[i]));
             }
         }
     }
     for (int j = 0; j < m; ++j) {
         int pos = 0;
-        int b =0;
+        int val =0;
         std::cin >> pos;
-        std::cin >> b;
+        std::cin >> val;
         auto eater = frogs.lower_bound(pos);
         int new_m = 1;
         while (new_m == 1) {
-            if (frogs.rbegin()->second < pos) {
-                mosquitoes.insert(std::make_pair(pos, b));
+            // bigger than last frog tongue or smaller than the first frog
+            int start_first = frogs.begin()->first;
+            int end_last = frogs.rbegin()->first + frogs.rbegin()->second.length;
+            if (end_last < pos || start_first > pos) {
+                mosquitoes.insert(std::make_pair(pos, val));
                 new_m = 0;
             } else {
                 if (eater == frogs.end() || eater->first > pos) {
-                    --eater;
+                    eater = std::prev(eater);
                 }
-                if(eater->second < pos){
-                    mosquitoes.insert(std::make_pair(pos, b));
+                if(eater->first + eater->second.length < pos){
+                    mosquitoes.insert(std::make_pair(pos, val));
                     break;
                 }
-                auto ipos = key_val.find(eater->first)->second;
-                eaten[ipos].first += 1;
-                eaten[ipos].second += b;
-                eater->second += b;
+                eaten[eater->second.number].first += 1;
+                eaten[eater->second.number].second += val;
+                eater->second.length += val;
                 auto next = frogs.upper_bound(eater->first);
-                while (next != frogs.end() && next->first <= eater->second) {
-                    if (next->second < eater->second) {
+                while (next != frogs.end() && next->first <= eater->first + eater->second.length) {
+                    if (next->first +next->second.length <= eater->first + eater->second.length) {
                         frogs.erase(next->first);
                     } else {
-                        auto elem = std::make_pair(eater->second + 1, next->second);
+                        frog newf;
+                        newf.pos = eater->first + eater->second.length + 1;
+                        newf.length = next->second.length - (eater->first + eater->second.length + 1 - next->first);
+                        newf.number = next->second.number;
+                        auto elem = std::make_pair(eater->first + eater->second.length + 1, newf);
                         frogs.erase(next->first);
                         frogs.insert(elem);
                     }
                     next = frogs.upper_bound(eater->first);
                 }
-                auto new_mos = mosquitoes.upper_bound(eater->first);
-                if (new_mos != mosquitoes.cend() && new_mos->first <= eater->second){
+                auto new_mos = mosquitoes.lower_bound(eater->first);
+                if (new_mos != mosquitoes.cend() && new_mos->first <= eater->first + eater->second.length){
                     pos = new_mos->first;
-                    b = new_mos->second;
+                    val = new_mos->second;
                     mosquitoes.erase(new_mos->first);
                 }else{
                     new_m = 0;
