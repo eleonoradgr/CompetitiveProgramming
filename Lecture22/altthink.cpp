a320101af352b748cbeb01ccc150fe393fd7d92a
eleonora <eleonora.digregorio8@gmail.com>
2021-01-15 11:18:51 +0100

    alternative thinking

---
+++ Lecture22/altthink.cpp
@@ -0,0 +1,23 @@
+//
+// Created by eleonora on 14/01/21.
+//
+
+#include <string>
+#include <iostream>
+#include <vector>
+
+int main(){
+    int n;
+    std::string s;
+    std::cin >> n >> s;
+    int len = 1;
+    for(int i = 1; i<n; ++i){
+        len += (s[i] != s[i-1]);
+    }
+    if (len  <=n-2) len+=2;
+    else {
+        if (len == n-1) len+=1;
+    }
+
+    std::cout << len;
+}
